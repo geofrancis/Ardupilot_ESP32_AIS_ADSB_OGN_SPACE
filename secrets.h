509413b7ac56566ecf8cf89acd96085691c61830
geofrancis <o.o.o.o@hotmail.co.uk>
2026-06-23 23:56:57 +0100

    Update secrets.h

--- secrets.h
+++ secrets.h
@@ -8,11 +8,6 @@ const char* WIFI_PASS_1 = "password";
 const char* WIFI_SSID_2 = "2.4";
 const char* WIFI_PASS_2 = "password";
 
-
-
-// --- Network Infrastructure ---
-const char* OGN_HOST = "aprs.glidernet.org";
-
 // --- API Keys ---
 const char* AIS_API_KEY = "*************************";
 const String N2YO_API_KEY = "**********************";
