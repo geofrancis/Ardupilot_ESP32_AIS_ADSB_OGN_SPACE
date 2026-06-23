1d5c402f7e9141ee9b089f0b9cc117a43da556a8
geofrancis <o.o.o.o@hotmail.co.uk>
2026-06-23 23:50:39 +0100

    Add secrets.h for Wi-Fi credentials and API keys

---
+++ secrets.h
@@ -0,0 +1,20 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// --- Wi-Fi Credentials ---
+// Primary and Backup Network Profiles
+const char* WIFI_SSID_1 = "2.4";
+const char* WIFI_PASS_1 = "password";
+const char* WIFI_SSID_2 = "2.4";
+const char* WIFI_PASS_2 = "password";
+
+
+
+// --- Network Infrastructure ---
+const char* OGN_HOST = "aprs.glidernet.org";
+
+// --- API Keys ---
+const char* AIS_API_KEY = "*************************";
+const String N2YO_API_KEY = "**********************";
+
+#endif
